7dfa57575d7e9f1c7d8a32893c17c8ccc40e003a
s1ty0 <138983006+s1ty0@users.noreply.github.com>
2025-07-01 15:40:10 +0800

    Add files via upload

---
+++ TS-Lib/data_process.py
@@ -0,0 +1,257 @@
+import numpy as np
+import torch
+
+from tools import read_parquet_file_kepler, get_std_data, build_patch_data_tess, save_as_numpy, build_patch_data_kepler
+
+# """
+#     一、Kepler dataset build.
+# """
+print("Begin is the Kepler Light Curve.")
+print("======================================")
+print("Kepler train data build...")
+
+train_data_dir = r"raw_data/kepler/train-00000-of-00001.parquet"
+save_dir_train = "dataset_k/train"
+
+light_curve_train, label_train, batch_res = build_patch_data_kepler(train_data_dir, save_dir_train)
+print("Kepler train dataset X's shape is ", light_curve_train.shape)
+print("Kepler train dataset y's shape is ", label_train.shape)
+print("Shape is: (patch_num, dim, d_model)")
+
+np_array = np.array(batch_res)
+print("batch_res: ", np_array.shape)
+print("sum: ", sum(batch_res))
+np.save('dataset_k12/patch_ids_train.npy', np_array)
+del light_curve_train, label_train
+
+print("======================================")
+print("Kepler test data build...")
+test_data_dir = r"raw_data/kepler/test-00000-of-00001.parquet"
+save_dir_test = "dataset_k/test"
+
+light_curve_test, label_test, batch_res = build_patch_data_kepler(test_data_dir, save_dir_test)
+print("Kepler test dataset X's shape is ", light_curve_test.shape)
+print("Kepler test dataset y's shape is ", label_test.shape)
+print("Shape is: (patch_num, dim, d_model)")
+
+np_array = np.array(batch_res)
+print("batch_res: ", np_array.shape)
+print("sum: ", sum(batch_res))
+np.save('dataset_k12/patch_ids_test.npy', np_array)
+del light_curve_test, label_test
+
+print("======================================")
+print("Kepler val data build...")
+val_data_dir = r"raw_data/kepler/validation-00000-of-00001.parquet"
+save_dir_val = "dataset_k/val"
+
+light_curve_val, label_val, batch_res = build_patch_data_kepler(val_data_dir, save_dir_val)
+print("Kepler val dataset X's shape is ", light_curve_val.shape)
+print("Kepler val dataset y's shape is ", label_val.shape)
+print("Shape is: (patch_num, dim, d_model)")
+
+np_array = np.array(batch_res)
+print("batch_res: ", np_array.shape)
+print("sum: ", sum(batch_res))
+np.save('dataset_k12/patch_ids_val.npy', np_array)
+del light_curve_val, label_val
+
+print("======================================")
+print("Kepler dataset which saved in dataset_k built over!")
+
+"""
+    二、Tess dataset build. # attn. wo choose 2 source data to build trian, val and test.
+"""
+print("======================================")
+print("Now is the Tess Light Curve.")
+print("======================================")
+print("Load and process data source1")
+val_data_path = r"raw_data/tess/valid.pt"
+val_lc, val_label, batch_res = build_patch_data_tess(val_data_path, save_dir = "dataset_t/val")
+
+np_array = np.array(batch_res)
+print("batch_res: ", np_array.shape)
+print("sum: ", sum(batch_res))
+np.save('dataset_t12/patch_ids_source1.npy', np_array)
+
+print("======================================")
+print("Data source1 X's shape is ", val_lc.shape)
+print("Data source1 y's shape is ", val_label.shape)
+print("Shape is: (patch_num, dim, d_model)")
+
+print("======================================")
+print("Load and process data source2")
+test_data_path = r"raw_data/tess/test.pt"
+test_lc, test_label, batch_res = build_patch_data_tess(test_data_path, save_dir="dataset_t/test")
+
+np_array = np.array(batch_res)
+print("batch_res: ", np_array.shape)
+print("sum: ", sum(batch_res))
+np.save('dataset_t12/patch_ids_source2.npy', np_array)
+
+print("======================================")
+print("Data source2 X's shape is ", test_lc.shape)
+print("Data source2 y's shape is ", test_label.shape)
+print("Shape is: (patch_num, dim, d_model)")
+
+print("======================================")
+print("Build test data from source 1 & 2 data")
+# build test data
+take_num = len(test_lc)
+val_num = len(val_lc)
+sample_num = take_num + val_num
+
+train_num = int(sample_num * 0.6)
+test_num = int(sample_num * 0.3)
+val_num = sample_num - train_num - test_num
+print("train samples: ", train_num)
+print("test samples: ", test_num)
+print("val samples: ", val_num)
+
+data_lc_test = test_lc[:test_num]  # 取出要叠加的数据
+data_label_test = test_label[:test_num]  # 取出要叠加的数据
+save_as_numpy(data_lc_test, data_label_test, save_dir="dataset_t/test")
+
+print("Build val data from source 1 & 2 data")
+data_lc_val = val_lc[:val_num]
+data_label_val = val_label[:val_num]
+save_as_numpy(data_lc_val, data_label_val, save_dir="dataset_t/val")
+
+print("Build train data from source 1 & 2 data")
+left_lc_test = test_lc[test_num:]
+left_label_test = test_label[test_num:]
+left_lc_val = val_lc[val_num:]
+left_label_val = val_label[val_num:]
+
+train_lc = np.vstack([left_lc_val, left_lc_test])  # Overlay
+train_label = np.vstack([left_label_val, left_label_test])
+train_lc = torch.from_numpy(train_lc)
+train_label = torch.from_numpy(train_label)
+save_as_numpy(train_lc, data_label_val, save_dir="dataset_t/train")
+print("Train data X's shape is ", train_lc.shape)
+print("Train data y's shape is ", train_label.shape)
+print("Test data X's shape is ", data_lc_test.shape)
+print("Test data y's shape is ", data_label_test.shape)
+print("Val data X's shape is ", data_lc_val.shape)
+print("Val data y's shape is ", data_label_val.shape)
+print("Shape is: (patch_num, dim, d_model)")
+print("Tess dataset which saved in dataset_t built over!")
+print("======================================")
+
+
+print("======================================")
+print("Balance data....................................")
+test_lc_path = 'dataset_t/test/lc_data.npy'
+test_label_path = 'dataset_t/test/label_data.npy'
+
+val_lc_path = 'dataset_t/val/lc_data.npy'
+val_label_path = 'dataset_t/val/label_data.npy'
+
+train_lc_path = 'dataset_t/train/lc_data.npy'
+train_label_path = 'dataset_t/train/label_data.npy'
+
+print("Balance val data....................................")
+val_lc = np.load(val_lc_path)
+val_label = np.load(val_label_path)
+
+count_0 = np.count_nonzero(val_label == 0)
+count_1 = np.count_nonzero(val_label == 1)
+
+negative_indices = np.where(val_label == 0)[0]
+positive_indices = np.where(val_label == 1)[0]
+
+target_negatives = int(len(positive_indices) * 1.3)
+
+selected_negatives = negative_indices[:target_negatives]
+
+selected_indices = np.concatenate([selected_negatives, positive_indices])
+
+balanced_lc = val_lc[selected_indices]
+balanced_label = val_label[selected_indices]
+
+new_count_0 = np.count_nonzero(balanced_label == 0)
+new_count_1 = np.count_nonzero(balanced_label == 1)
+print(f"Negative sample count: {new_count_0}, positive sample count: {new_count_1}")
+
+np.save(val_lc_path, balanced_lc)
+np.save(val_label_path, balanced_label)
+
+del val_lc, val_label
+del balanced_lc, balanced_label
+del selected_indices, selected_negatives
+del negative_indices, positive_indices
+
+print("Balance test data....................................")
+val_lc = np.load(test_lc_path)
+val_label = np.load(test_label_path)
+
+count_0 = np.count_nonzero(val_label == 0)
+count_1 = np.count_nonzero(val_label == 1)
+
+negative_indices = np.where(val_label == 0)[0]
+positive_indices = np.where(val_label == 1)[0]
+
+target_negatives = int(len(positive_indices) * 1.3)
+
+selected_negatives = negative_indices[:target_negatives]
+
+selected_indices = np.concatenate([selected_negatives, positive_indices])
+
+balanced_lc = val_lc[selected_indices]
+balanced_label = val_label[selected_indices]
+
+new_count_0 = np.count_nonzero(balanced_label == 0)
+new_count_1 = np.count_nonzero(balanced_label == 1)
+print(f"Negative sample count: {new_count_0}, positive sample count: {new_count_1}")
+
+np.save(test_lc_path, balanced_lc)
+np.save(test_label_path, balanced_label)
+
+del val_lc, val_label
+del balanced_lc, balanced_label
+del selected_indices, selected_negatives
+del negative_indices, positive_indices
+
+print("Balance train data....................................")
+val_lc = np.load(train_lc_path)
+val_label = np.load(train_label_path)
+
+count_0 = np.count_nonzero(val_label == 0)
+count_1 = np.count_nonzero(val_label == 1)
+
+negative_indices = np.where(val_label == 0)[0]
+positive_indices = np.where(val_label == 1)[0]
+
+target_negatives = int(len(positive_indices) * 1.3)
+
+selected_negatives = negative_indices[:target_negatives]
+
+selected_indices = np.concatenate([selected_negatives, positive_indices])
+
+balanced_lc = val_lc[selected_indices]
+balanced_label = val_label[selected_indices]
+
+new_count_0 = np.count_nonzero(balanced_label == 0)
+new_count_1 = np.count_nonzero(balanced_label == 1)
+print(f"Negative sample count: {new_count_0}, positive sample count: {new_count_1}")
+
+print("train data shape is: ", balanced_lc.shape)
+np.save(train_lc_path, balanced_lc)
+np.save(train_label_path, balanced_label)
+
+del val_lc, val_label
+del balanced_lc, balanced_label
+del selected_indices, selected_negatives
+del negative_indices, positive_indices
+
+del train_lc_path, train_label_path
+del test_lc_path, test_label_path
+del val_lc_path, val_label_path
+del count_0, count_1
+del new_count_0, new_count_1
+del target_negatives
+
+print("Work Over, now you have the final tess dataset with idea1 & idea2. ")
+print("And this dataset has been saved in dataset_t12/train test val")
+
+
---
+++ TS-Lib/tools.py
@@ -0,0 +1,408 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+import pyarrow.parquet as pq
+from tqdm import tqdm
+
+from transformers import AutoTokenizer, AutoModel
+
+from sklearn.decomposition import PCA
+
+def read_parquet_file_kepler(file_path):
+    parquet_file = pq.ParquetFile(file_path)
+    data = parquet_file.read().to_pandas()
+
+    light_curve = data.flux_norm
+    label = data.label
+    metadata = data.metadata
+
+    # # todo run
+    # light_curve = light_curve[0:512]
+    # label = label[0:512]
+
+    return light_curve, label, metadata
+
+# Function Definition - linear interpolate
+def linear_interpolate_numpy(t, y):
+    """
+    Fills missing values using linear interpolation.
+
+    :param t: Time array or index array
+    :param y: Light curve data, can be a numpy array, pandas Series, or torch.Tensor
+    :return: Interpolated light curve, maintaining the same data type as input
+    """
+    # 1. Determine the input data type is numpy
+    input_type = None
+    if isinstance(y, pd.Series):
+        input_type = 'series'
+        y_values = y.values
+        index = y.index
+    elif isinstance(y, torch.Tensor):
+        input_type = 'tensor'
+        y_values = y.numpy()
+    else:  # default
+        input_type = 'array'
+        y_values = y
+
+    # 2. Check if all values are NaN
+    if np.all(np.isnan(y_values)):
+        raise ValueError("所有数据点均为 NaN，无法进行插值")
+
+    # 3. Perform linear interpolation
+    mask = ~np.isnan(y_values)
+    t_valid = t[mask]
+    y_valid = y_values[mask]
+
+    # Handle the special case where t_valid has only one point (interpolation is not possible)
+    if len(t_valid) < 2:
+        # Fill with the only valid value
+        y_filled = np.full_like(y_values, y_valid[0])
+    else:
+        y_filled = np.interp(t, t_valid, y_valid)
+
+    # 4. Return results according to the original data type
+    if input_type == 'series':
+        return pd.Series(y_filled, index=index)
+    elif input_type == 'tensor':
+        return torch.from_numpy(y_filled)
+    else:
+        return y_filled
+
+# Function Definition - safe unfold
+def safe_unfold(res, dimension, size, step):
+    """
+    Safely performs the tensor unfold operation, returns None on failure.
+
+    Args:
+        res (torch.Tensor): Input tensor.
+        dimension (int): Dimension to unfold.
+        size (int): Size of each slice.
+        step (int): Step between consecutive slices.
+
+    Returns:
+        torch.Tensor or None: Unfolded tensor if successful, otherwise None.
+    """
+    try:
+        return res.unfold(dimension=dimension, size=size, step=step)
+    except Exception as e:
+        print(f"Unfold fail: {e}")
+        return None
+
+# Function Definition - patch build
+def create_patch(xb, patch_len, stride):
+    """
+    xb: [bs x seq_len x n_vars]
+    """
+    seq_len = xb.shape[1]
+    num_patch = (max(seq_len, patch_len) - patch_len) // stride + 1
+    tgt_len = patch_len + stride * (num_patch - 1)
+    res = xb[:, :tgt_len, :]  # xb: [bs x tgt_len x nvars]
+
+    lc_patch = safe_unfold(res, dimension=1, size=patch_len, step=stride)
+
+    if lc_patch is None:
+        print(f"patch_len={patch_len}, stride={stride} contribute to unfold operation failed，pass")
+        return None, None
+
+    return lc_patch, num_patch
+
+batch_res = []
+# Function Definition - Input & Output Data Matching Function
+def match_data(lc, label, patch_len, pred_len, stride):
+    lc_patch, num_patch = create_patch(lc, patch_len, stride)
+
+    label_start = pred_len
+    label_end = lc.shape[1]
+
+    label_valid = label[:, label_start:label_end, :]
+    label_patch, _ = create_patch(label_valid, pred_len, stride) # 步幅一致
+
+    global batch_res # get batch_ids
+    if label_patch.shape[1] <= num_patch:  # choose minner
+        lc_patch = lc_patch[:, :label_patch.shape[1], :, :]
+        batch_res.append(label_patch.shape[1])
+    else:
+        label_patch = label_patch[:, :num_patch, :, :]
+        batch_res.append(num_patch)
+
+    label_patch = torch.any(label_patch == 1, dim=3, keepdim=True).float()
+    return lc_patch, label_patch
+
+
+# Function Definition - data save
+def save_as_numpy(lc_tensor, label_tensor, save_dir="./"):
+    os.makedirs(save_dir, exist_ok=True)
+
+    if isinstance(lc_tensor, torch.Tensor):
+        np.save(f"{save_dir}/lc_data.npy", lc_tensor.cpu().numpy())
+        np.save(f"{save_dir}/label_data.npy", label_tensor.cpu().numpy())
+    else:
+        np.save(f"{save_dir}/lc_data.npy", lc_tensor)
+        np.save(f"{save_dir}/label_data.npy", label_tensor)
+    print(f"Data has saved in {save_dir}")
+
+
+# Function Definition - process data
+def get_std_data(lc_series, label_series, patch_len=512, batch_size=8, pred_len=48, stride=24, save_dir="./"):
+    # Three hyperparameters. The default values are the parameters in the original FLARE paper
+    patch_len = patch_len
+    stride = stride
+    pred_len = pred_len
+
+    total_samples = len(lc_series)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    all_lc_batches = []
+    all_label_batches = []
+
+    # Process data in batches
+    for batch_idx in range(0, total_samples, batch_size):
+        # Get the data of the current batch
+        batch_lc = lc_series[batch_idx:batch_idx + batch_size]
+        batch_label = label_series[batch_idx:batch_idx + batch_size]
+
+        batch_lc_patches = []
+        batch_label_patches = []
+
+        # Process each sample within the batch
+        for lc, label in zip(batch_lc, batch_label):
+            index = np.arange(len(lc))
+            lc = linear_interpolate_numpy(index, lc)
+
+            # Convert to tensor and adjust the shape
+            lc_torch = torch.from_numpy(lc)
+            lc_input = lc_torch.view(1, -1, 1).to(device)
+            label_torch = torch.from_numpy(label)
+            label_input = label_torch.view(1, -1, 1).to(device)
+
+            # 生成patch
+            lc_patch, label_patch = match_data(lc_input, label_input, patch_len, pred_len, stride)
+            batch_lc_patches.append(lc_patch.squeeze(0))  # 去除第0维
+            batch_label_patches.append(label_patch.squeeze(0))
+
+        # Concatenate all patches of the current batch
+        batch_lc_tensor = torch.cat(batch_lc_patches, dim=0)
+        batch_label_tensor = torch.cat(batch_label_patches, dim=0)
+
+        # Store the results of the current batch
+        all_lc_batches.append(batch_lc_tensor)
+        all_label_batches.append(batch_label_tensor)
+
+        # Clear the gpu memory
+        del batch_lc_patches, batch_label_patches, batch_lc_tensor, batch_label_tensor
+        torch.cuda.empty_cache()
+
+    # Finally, splice the results of all batches
+    final_lc_tensor = torch.cat(all_lc_batches, dim=0)
+    final_label_tensor = torch.cat(all_label_batches, dim=0)
+
+    global batch_res
+    save_as_numpy(final_lc_tensor, final_label_tensor, save_dir=save_dir)
+    return final_lc_tensor, final_label_tensor, batch_res
+
+
+def build_patch_data_tess(data_dir, save_dir=""):
+    data = torch.load(data_dir, weights_only=False)
+
+    light_curve = data['flux']
+    label = data['mask']
+
+    # todo run
+    # light_curve = light_curve[0:512]
+    # label = label[0:512]
+
+    global batch_res
+    batch_res = []
+    light_curve_std, label_std, batch_res = get_std_data(light_curve, label, save_dir = f"./{save_dir}")
+    return light_curve_std, label_std, batch_res
+
+def build_patch_data_kepler(data_dir, save_dir=""):
+    global batch_res
+    batch_res = []
+
+    light_curve, label, _ = read_parquet_file_kepler(data_dir)  # get lc and label
+    light_curve_train, label_train, batch_res = get_std_data(light_curve, label, patch_len=512, pred_len=480, stride=48,
+                                                             save_dir=save_dir)
+    return light_curve_train, label_train, batch_res
+
+def get_idea1_txt_tess(data_dir, out_path):
+    data = torch.load(data_dir, weights_only=False)
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+
+    light_curve = data['flux']
+    label = data['mask']
+
+    # todo run
+    # light_curve = light_curve[0:512]
+    # label = label[0:512]
+
+    i = 0
+    mode = "w"
+    for (lc, lb) in zip(light_curve, label):
+        flux_median = np.median(lc)
+        count_ones = np.sum(lb)
+        line = f"For the star, a total of {count_ones} stellar flares occurred with a median flux of {flux_median}"
+        with open(out_path, mode) as f:
+            f.write(f"{line}\n")
+        i = i + 1
+        mode = "a"
+    print(f"Total deal {i} records have been written.") # to Match.
+
+def get_idea2_txt_kepler(data_dir, out_path):
+    light_curve, label, metadata = read_parquet_file_kepler(data_dir)
+    print(f"length: {len(label)}")
+
+    # todo run
+    # label = label[0:512]
+    # metadata = metadata[0:512]
+
+    i = 0
+    mode = "w"
+    for (lb, md) in zip(label, metadata):
+        kic_id = md['kic_id']
+        quarter = md['quarter']
+        record_list = [i + 1 for i, val in enumerate(lb) if val == 1]
+
+        line = f"For the star with ID {kic_id}, during observation quarter {quarter}, the historical time points of flare eruptions during observation quater {quarter} are: {record_list}."
+        with open(out_path, mode) as f:
+            f.write(f"{line}\n")
+        i = i + 1
+        mode = "a"
+
+    print(f"Total deal {i} records have been written.")  # to Match.
+
+
+def get_idea1_txt_kepler(data_dir, out_path):
+    light_curve, label, metadata = read_parquet_file_kepler(data_dir)
+    print(f"length: {len(label)}")
+
+    # todo run
+    # label = label[0:512]
+    # metadata = metadata[0:512]
+
+    i = 0
+    mode = "w"
+    for (lb, md) in zip(label, metadata):
+        kic_id = md['kic_id']
+        quarter = md['quarter']
+        flux_median = md['flux_median']
+        count_ones = np.sum(lb)
+
+        line = f"For the star with ID {kic_id}, during observation quarter {quarter}, a total of {count_ones} stellar flares occurred with a median flux of {flux_median}."
+        with open(out_path, mode) as f:
+            f.write(f"{line}\n")
+        i = i + 1
+        mode = "a"
+
+    print(f"Total deal {i} records have been written.")  # to Match.
+
+
+def get_idea2_txt_tess(data_dir, out_path):
+    data = torch.load(data_dir, weights_only=False)
+    label = data['mask']
+
+    # todo run
+    # label = label[0:512]
+
+    i = 0
+    mode = "w"
+    for lb in label:
+        record_list = [i + 1 for i, val in enumerate(lb) if val == 1]
+        line = f"For the star, the historical time points of flare eruptions during observation quarter are {record_list}"
+        with open(out_path, mode) as f:
+            f.write(f"{line}\n")
+        i = i + 1
+        mode = "a"
+
+    print(f"Total deal {i} records have been written.")  # to Match.
+
+
+# Function Definition - batch encode
+def batch_encode(texts, model, tokenizer, device, batch_size=32, max_length=128, pooling='cls'):
+    all_embeddings = []
+
+    for i in tqdm(range(0, len(texts), batch_size), desc="batch encode"):
+        batch_texts = texts[i:i+batch_size]
+
+        inputs = tokenizer(
+            batch_texts,
+            padding='max_length',
+            truncation=True,
+            max_length=max_length,
+            return_tensors='pt'
+        )
+
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        last_hidden = outputs.last_hidden_state  # [batch_size, seq_len, hidden_size]
+
+        if pooling == 'cls':
+            embeddings = last_hidden[:, 0, :]  # [CLS]标记的表示
+        elif pooling == 'mean':
+            attention_mask = inputs['attention_mask']
+            embeddings = (last_hidden * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1, keepdim=True)
+
+        all_embeddings.append(embeddings.cpu().numpy())
+
+    return np.vstack(all_embeddings)
+
+def get_emb_from_txt(out_path_txt, out_path):
+    print("======================================")
+    print("Using Bert Encoder to encode our texts.")
+    with open(out_path_txt, 'r', encoding='utf-8') as f: 
+        texts = f.readlines()
+    texts = [text.strip() for text in texts] # # 简单预处理（去除换行符）
+    # print(texts) # debug
+    print(f"Load {len(texts)} records.")
+    model_name = 'bert-base-uncased' # for English(base) # Add some exp.
+    # model_name = 'bert-base-chinese'                # for Chinese(base)
+    # model_name = 'hfl/chinese-bert-wwm'           # BERT pre-trained on Chinese Wikipedia
+    # model_name = 'hfl/chinese-roberta-wwm-ext'    # Enhanced Chinese RoBERTa
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    model.eval()  # 设置为评估模式
+
+    embeddings = batch_encode(
+        texts=texts,
+        model=model,
+        tokenizer=tokenizer,
+        device=device,
+        batch_size=32,
+        max_length=64,
+        pooling='cls'
+    )
+    print(f"768 shape is: {embeddings.shape}")
+    print("Using PCA to turn 768 into 512.")
+
+    pca = PCA(n_components=512) # todo run
+    vectors_512 = pca.fit_transform(embeddings)
+    print(f"Before: {embeddings.shape}")
+    print(f"After PCA: {vectors_512.shape}")
+    explained_variance = pca.explained_variance_ratio_.sum()
+    print(f"Proportion of explained variance retained: {explained_variance:.4f}")
+
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    np.save(out_path, vectors_512)
+
+def read_parquet_file_kepler(file_path): #
+    parquet_file = pq.ParquetFile(file_path)
+    data = parquet_file.read().to_pandas()
+
+    light_curve = data.flux_norm
+    label = data.label
+    metadata = data.metadata
+
+    # todo run
+    # light_curve = light_curve[0:512]
+    # label = label[0:512]
+    # metadata = metadata[0:512]
+
+    return light_curve, label, metadata
